6c2748f0b2c8adf71ce610c017585dbc95bf1223
Julian Villari <julianvillari@gmail.com>
2022-10-06 20:51:21 -0300

    Clone Repositorio

---
+++ app.js
@@ -0,0 +1,30 @@
+import { validarProductoRepetido } from "./src/accionesCarrito.js";
+
+const mostrarProductos = (productos) => {
+    const contenedorProductos = document.getElementById('contedor-productos')
+
+productos.forEach(producto => {
+    const div = document.createElement('div')
+
+    div.classList.add('card')
+
+    div.innerHTML += `<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">${producto.nombre}</h5>
+      <p class="card-text">Descripcion: ${producto.desc}</p>
+      <p class="card-text">Precio: ${producto.precio}</p>
+      <a class="btn btn-primary" id=boton${producto.id}>Comprar</a>
+    </div>
+  </div>`
+  contenedorProductos.appendChild(div);
+
+  const boton = document.getElementById(`boton${producto.id}`);
+
+  boton.addEventListener(`click`, ()=> {
+    // alert(`Se agrego ${producto.nombre} al carrito`)
+    validarProductoRepetido(producto.id);
+  })
+});
+};
+export { mostrarProductos };
---
+++ src/stock.js
@@ -0,0 +1,52 @@
+const productos = [
+
+    {
+      id: 1,
+      nombre: "Wooly Premium",
+      precio: 85000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/wollypremium.jpg",
+      cant: 1
+    },
+    {
+      id: 2,
+      nombre: "Emuli Pro",
+      precio: 80000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/emulipro.jpg",
+      cant: 1
+    },
+    {
+      id: 3,
+      nombre: "Emuli Bow",
+      precio: 75000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/emulibow.jpg",
+      cant: 1
+    },
+    {
+      id: 4,
+      nombre: "Nile Classic",
+      precio: 70000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/nileclassic.jpg",
+      cant: 1
+    },
+    {
+      id: 5,
+      nombre: "Wit 100",
+      precio: 90000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/wit100.jpg",
+      cant: 1
+  },
+  {
+      id: 6,
+      nombre: "Wit 3D 100",
+      precio: 95000,
+      desc: "Palo de hockey Vlack",
+      img: "public/images/wit3d100.jpg",
+      cant: 1
+  }
+];
+export { productos };
