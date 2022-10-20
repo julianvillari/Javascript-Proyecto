f22151bc36b3ed12b3100e52aba5459373945abd
Julian Villari <julianvillari@gmail.com>
2022-10-20 18:20:46 -0300

    Cree stock.json, y obtenerProductos

--- src/accionesCarrito.js
+++ src/accionesCarrito.js
@@ -38,7 +38,7 @@ const agregarAlCarrito = (productoId) => {
     const div = document.createElement('div');
     div.classList.add('productoEnCarrito');
     div.innerHTML = `<p>${producto.nombre}</p>
-                    <p>Precio: ${producto.precio}</p>
+                    <p>Precio: $${producto.precio}</p>
                     <p id=cantidad${producto.id}>Cantidad: ${producto.cantidad}</p>
                     <button id=eliminar${producto.id} value='${producto.id}' class='btn waves-effect waves-ligth boton-eliminar'>X</button>
                     `;
